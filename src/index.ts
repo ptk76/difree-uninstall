b3bcc81cca82193db88d46dd468ba9adf5d286a0
Przemek Kudla <przemek@technologie21.info>
2023-12-11 22:25:52 +0100

    disableGeoip: false

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const POSTHOG_KEY = "phc_9Tyn6okdZz8WGrwv4B5dfOU5pEwox04bwRd0Nt1Wdlw";
 const params = new URLSearchParams(window.location.search);
 const uuid = params.has("uuid") ? params.get("uuid") : "UNKNOWN";
 const client = new PostHog(POSTHOG_KEY, { host: "https://eu.posthog.com" });
-client.capture({ distinctId: uuid, event: "uninstall" });
+client.capture({ distinctId: uuid, event: "uninstall", disableGeoip: false });
 client.shutdownAsync();
 
 setTimeout(() => {
